93eb5e931076f5c35abc0578486e967a941fd190
Tom Murua <tommurua@gmail.com>
2022-12-19 01:46:20 -0300

    add index.html, style.css and app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('schedule-form');
+const table = document.getElementById('schedule-table');
+
+function sortTable() {
+  // Get all rows in the table
+  const rows = Array.from(table.rows).slice(1);  // slice(1) to exclude the header row
+
+  // Sort the rows based on the time value
+  rows.sort((row1, row2) => {
+    const time1 = row1.cells[1].innerHTML;
+    const time2 = row2.cells[1].innerHTML;
+    return time1 > time2 ? 1 : -1;
+  });
+
+  // Add the sorted rows back to the table
+  rows.forEach((row) => table.appendChild(row));
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const taskName = document.getElementById('task-name').value;
+  const taskTime = document.getElementById('task-time').value;
+
+  const row = table.insertRow(-1);
+  const nameCell = row.insertCell(0);
+  const timeCell = row.insertCell(1);
+  const deleteCell = row.insertCell(2);
+
+  nameCell.innerHTML = taskName;
+  timeCell.innerHTML = taskTime;
+  deleteCell.innerHTML = '<button class="delete-button">Delete</button>';
+
+  form.reset();
+
+  sortTable();
+});
+
+table.addEventListener('click', (event) => {
+  if (event.target.className === 'delete-button') {
+    event.target.parentNode.parentNode.remove();
+    sortTable();
+  }
+});
